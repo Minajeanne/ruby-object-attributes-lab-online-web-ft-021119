9bf503feed581cf6c409ec46b202573e42a6f68f
Alicia Santiago <albrown07@mac.com>
2019-02-18 16:28:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,10 @@
 class Dog 
   
-  def initialize(name)
-    @name = name 
-  end
-  
   def name
     @name
   end
   
-  def name=(new_name)
+  def name=(dogs_name)
     @name = new_name
   end
   
